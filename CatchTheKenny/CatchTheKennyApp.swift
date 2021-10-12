1df4e87512ada45ec088720e5a689c0ba572a1d0
selcuk baki <selcukbaki@windowslive.com>
2021-10-12 04:42:13 +0300

    Initial Commit

---
+++ CatchTheKenny/CatchTheKennyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchTheKennyApp.swift
+//  CatchTheKenny
+//
+//  Created by Selcuk Baki on 12/10/21.
+//
+
+import SwiftUI
+
+@main
+struct CatchTheKennyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
