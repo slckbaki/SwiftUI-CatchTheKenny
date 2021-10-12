d67957267bdba7bbd061d2e990c296140fc3a636
selcuk baki <selcukbaki@windowslive.com>
2021-10-12 05:33:45 +0300

    firs view

--- CatchTheKenny/ContentView.swift
+++ CatchTheKenny/ContentView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var score = 0
+    @State var timeLeft = 10.0
+    @State var chosenX = 180
+    @State var chosenY = 250
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Text("Count: ")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+            Text("Points: \(score)")
+            Text("Time Left: \(timeLeft)")
+                .frame(alignment: .leading)
+                .padding(.bottom, 100)
+        Image("panda")
+            .resizable()
+            .frame(width: 120, height: 120, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            .position(x: CGFloat(chosenX), y: CGFloat(chosenY))
+        Spacer()
+        }
     }
 }
 
